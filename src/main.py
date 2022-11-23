cc38f923ac63d810f903d940656407fdd1a0e408
Simon Xu <simonxhy0404@gmail.com>
2022-11-22 22:56:56 -0600

    darkened background
    added gray background to end screen

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
-import os
-import random
-import sys
-
 import pygame as pg
 import pygame.freetype
+
+import random
+import sys
 
 from src.player import Player
 from src.log import Log
@@ -16,7 +15,7 @@ clock = pg.time.Clock()
 FPS = 60
 
 
-def end(world, message, font_big, font_small, difficulty):
+def end(world, message, font_title, font_desc, difficulty):
     difficulty_str = ""
     if difficulty == 1:
         difficulty_str = "EASY"
@@ -36,25 +35,27 @@ def end(world, message, font_big, font_small, difficulty):
                 if event.key == ord('r'):
                     main(difficulty)
                 if event.key == ord('c'):
-                    changeDifficulty(world, font_big, font_small)
+                    changeDifficulty(world, font_title, font_desc)
 
         if "LOSE" in message:
-            message_surf, message_rect = font_big.render(message, (255, 0, 0))
+            message_surf, message_rect = font_title.render(message, (255, 0, 0))
         else:
-            message_surf, message_rect = font_big.render(message + " - " + difficulty_str, (0, 255, 0))
+            message_surf, message_rect = font_title.render(message + " - " + difficulty_str, (0, 255, 0))
 
         message_rect.x = world.get_width() / 2 - message_rect.width / 2
-        message_rect.y = world.get_height() / 2 - message_rect.height / 2
+        message_rect.y = world.get_height() / 3 - message_rect.height / 2
 
-        play_again_surf, play_again_rect = font_small.render('PRESS "R" TO PLAY AGAIN', (255, 255, 255))
+        play_again_surf, play_again_rect = font_desc.render('PRESS "R" TO PLAY AGAIN', (255, 255, 255))
         play_again_rect.x = world.get_width() / 2 - play_again_rect.width / 2
         play_again_rect.y = 3 * world.get_height() / 4 - play_again_rect.height / 2
 
-        difficulty_surf, difficulty_rect = font_small.render('PRESS "C" TO CHANGE DIFFICULTY', (255, 255, 255))
+        difficulty_surf, difficulty_rect = font_desc.render('PRESS "C" TO CHANGE DIFFICULTY', (255, 255, 255))
         difficulty_rect.x = world.get_width() / 2 - difficulty_rect.width / 2
         difficulty_rect.y = 4 * world.get_height() / 5 - difficulty_rect.height / 2
 
-        world.fill((0, 0, 1))
+        bg = pg.image.load('../assets/bg_gray.png')
+
+        world.blit(bg, world.get_rect())
         world.blit(message_surf, message_rect)
         world.blit(play_again_surf, play_again_rect)
         world.blit(difficulty_surf, difficulty_rect)
@@ -62,7 +63,7 @@ def end(world, message, font_big, font_small, difficulty):
         pg.display.flip()
 
 
-def changeDifficulty(world, font_big, font_small):
+def changeDifficulty(world, font_title, font_desc):
     difficulty = 1
     while True:
         pg.time.delay(100)
@@ -81,19 +82,19 @@ def changeDifficulty(world, font_big, font_small):
                 if event.key == ord('r'):
                     main(difficulty)
 
-        title_surf, title_rect = font_big.render('LORD OF THE FIRE', (255, 0, 0))
+        title_surf, title_rect = font_title.render('LORD OF THE FIRE', (255, 0, 0))
         title_rect.x = world.get_width() / 2 - title_rect.width / 2
         title_rect.y = world.get_height() / 3 - title_rect.height / 2
 
-        author_surf, author_rect = font_small.render("BY AIDEN FARLEY, SIMON XU, BOWEN YANG", (255, 255, 255))
+        author_surf, author_rect = font_desc.render("BY AIDEN FARLEY, SIMON XU, BOWEN YANG", (255, 255, 255))
         author_rect.x = world.get_width() / 2 - author_rect.width / 2
         author_rect.y = 3 * world.get_height() / 7 - author_rect.height / 2
 
-        start_surf, start_rect = font_small.render('PRESS "R" TO START', (255, 255, 255))
+        start_surf, start_rect = font_desc.render('PRESS "R" TO START', (255, 255, 255))
         start_rect.x = world.get_width() / 2 - start_rect.width / 2
         start_rect.y = 3 * world.get_height() / 4 - start_rect.height / 2
 
-        difficulty_surf, difficulty_rect = font_small.render('"E" - EASY  "M" - MEDIUM  "H" - HARD', (255, 255, 255))
+        difficulty_surf, difficulty_rect = font_desc.render('"E" - EASY  "M" - MEDIUM  "H" - HARD', (255, 255, 255))
         difficulty_rect.x = world.get_width() / 2 - difficulty_rect.width / 2
         difficulty_rect.y = 4 * world.get_height() / 5 - difficulty_rect.height / 2
 
@@ -112,7 +113,8 @@ def main(difficulty):
     log_decay_rate = 120
     log_count = 0
     fire_log_count = 10
-    font_big = pg.freetype.Font("../assets/font.TTF", 48)
+    font_big = pg.freetype.Font("../assets/font.TTF", 62)
+    font_med = pg.freetype.Font("../assets/font.TTF", 48)
     font_small = pg.freetype.Font("../assets/font.TTF", 24)
 
     bg = pg.image.load("../assets/bg.png")
@@ -206,8 +208,8 @@ def main(difficulty):
         logs.draw(world)
         logs.update()
 
-        log_counter = Text(str(log_count), font_big, (0, 0, 0), width - 30, height - 100)
-        fire_counter = Text(str(fire_log_count), font_big, (0, 0, 0), width - 30, height - 150)
+        log_counter = Text(str(log_count), font_med, (0, 0, 0), width - 30, height - 100)
+        fire_counter = Text(str(fire_log_count), font_med, (0, 0, 0), width - 30, height - 150)
 
         texts.add(fire_counter)
         texts.add(log_counter)
